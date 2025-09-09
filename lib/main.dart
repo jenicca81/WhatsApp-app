cce6b1f18d855652797dcbd23b28b46d9c887dd5
Pambaniso Bongiwe <218008159@stud.cut.ac.za>
2025-09-09 17:36:25 +0200

    added more widgets

--- lib/main.dart
+++ lib/main.dart
@@ -12,159 +12,169 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('WhatsApp'),
-          backgroundColor: Colors.green,
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: Text('WhatsApp'),
+            backgroundColor: Colors.green,
 
-          actions: [
-            IconButton(
-              onPressed: () {
-                print("Search tapped");
-              },
-              icon: Icon(Icons.search),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  print("Search tapped");
+                },
+                icon: Icon(Icons.search),
+              ),
+              IconButton(
+                onPressed: () {
+                  print("Chat icon tapped");
+                },
+                icon: Icon(Icons.chat),
+              ),
+              IconButton(
+                onPressed: () {
+                  print("call tapped");
+                },
+                icon: Icon(Icons.call),
+              ),
+            ],
+            bottom: TabBar(
+              tabs: [
+                Tab(text: "status"),
+                Tab(text: "Chats"),
+                Tab(text: "Calls"),
+              ],
             ),
-            IconButton(
-              onPressed: () {
-                print("Chat icon tapped");
-              },
-              icon: Icon(Icons.chat),
-            ),
-            IconButton(
-              onPressed: () {
-                print("call tapped");
-              },
-              icon: Icon(Icons.call),
-            ),
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(color: Colors.grey),
-                child: Text(
-                  "Pambaniso Bongi",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.grey),
+                  child: Text(
+                    "Pambaniso Bongi",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                    ),
                   ),
                 ),
-              ),
+                ListTile(
+                  leading: Icon(Icons.home_filled),
+                  title: Text("Account"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text("Settings"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.help_center),
+                  title: Text("Help"),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+          body: ListView(
+            children: [
               ListTile(
-                leading: Icon(Icons.home_filled),
-                title: Text("Account"),
-                onTap: () {},
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage("assets/profile1.jpg"),
+                ),
+                title: Text(
+                  "Jessica",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  "Ka theowela ka 10",
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
               ),
+              Divider(),
               ListTile(
-                leading: Icon(Icons.settings),
-                title: Text("Settings"),
-                onTap: () {},
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage("assets/profile2.jpg"),
+                ),
+                title: Text(
+                  "Bobe",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  "Hareye KFC",
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
               ),
+              Divider(),
               ListTile(
-                leading: Icon(Icons.help_center),
-                title: Text("Help"),
-                onTap: () {},
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage("assets/profile3.jpg"),
+                ),
+                title: Text(
+                  "daki",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  "Apply moo",
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
+              Divider(),
+              ListTile(
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage("assets/profile2.jpg"),
+                ),
+                title: Text(
+                  "Mmami",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  "Aya o hopotse Nono",
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
+              Divider(),
+              ListTile(
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage("assets/profile3.jpg"),
+                ),
+                title: Text(
+                  "Papa",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  "Send hee fast",
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
+              Divider(),
+              ListTile(
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage("assets/profile3.jpg"),
+                ),
+                title: Text(
+                  "Malefu",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  "ke Welkom",
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
               ),
             ],
           ),
-        ),
-        body: ListView(
-          children: [
-            ListTile(
-              leading: CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage("assets/profile1.jpg"),
-              ),
-              title: Text(
-                "Jessica",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                "Ka theowela ka 10",
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            Divider(),
-            ListTile(
-              leading: CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage("assets/profile2.jpg"),
-              ),
-              title: Text(
-                "Bobe",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                "Hareye KFC",
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            Divider(),
-            ListTile(
-              leading: CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage("assets/profile3.jpg"),
-              ),
-              title: Text(
-                "daki",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                "Apply moo",
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            Divider(),
-            ListTile(
-              leading: CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage("assets/profile2.jpg"),
-              ),
-              title: Text(
-                "Mmami",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                "Aya o hopotse Nono",
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            Divider(),
-            ListTile(
-              leading: CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage("assets/profile3.jpg"),
-              ),
-              title: Text(
-                "Papa",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                "Send hee fast",
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            Divider(),
-            ListTile(
-              leading: CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage("assets/profile3.jpg"),
-              ),
-              title: Text(
-                "Malefu",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                "ke Welkom",
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-          ],
         ),
       ),
     );
